e3b2addc45ec079fb5388aef5fb14dd79f266d66
Mike Sena <mikejsena84@gmail.com>
2022-04-15 15:19:50 -0500

    Added blog posts UL

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,32 @@
+//Step 1: Import React
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const BlogPage = () => {
+//Step 2: Define your component
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <p>My cool posts will go here</p>
+            <ul>
+            {
+                data.allFile.nodes.map(node => (
+                    <li key={node.name}>
+                        {node.name}
+                    </li>
+                ))
+            }    
+            </ul>
         </Layout>
     )
 }
-
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
+//Step 3: Export your content
 export default BlogPage
