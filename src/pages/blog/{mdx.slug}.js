a6fe94e65699203eeb3ca40b8ced64448632ee5e
Mike Sena <mikejsena84@gmail.com>
2022-04-19 16:05:52 -0500

    mid part 6

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+
+const BlogPost = ()=> {
+    return (
+        <Layout pageTitle="Super Cool Blog Posts">
+            <p>My blog postes contents will go here (eventually).</p>
+        </Layout>
+    )
+}
+
+export default BlogPost 
